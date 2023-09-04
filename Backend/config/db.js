5bea07b6f03f27b535dae1ece4a15a04d315a3c1
faisalpinitod <faisalpinitod@gmail.com>
2023-09-04 16:47:26 +0530

    completed

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 require('dotenv').config();
 
 const mongoURI = process.env.MONGO_URI;
-const connection=mongoose.connect(mongoURI)
+const connection=mongoose.connect('mongodb+srv://Faisalpinitod:faisal@cluster0.y2f7t.mongodb.net/fitlifeCompanion?retryWrites=true&w=majority')
 
 
 module.exports={
